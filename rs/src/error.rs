1c9571cb94ba18583b68b316583de86c0addac92
Arjun Guha <a.guha@northeastern.edu>
2020-11-25 06:35:06 -0500

    Error handling experiment

--- rs/src/error.rs
+++ rs/src/error.rs
@@ -10,6 +10,16 @@ pub enum CommandError {
     Exif(#[from] exif::Error),
     #[error("{0}")]
     Other(String),
+    #[error("{0}\n{1}")]
+    Trace(String, Box<CommandError>),
+}
+
+pub fn trace<E>(message: impl Into<String>) -> Box<dyn FnOnce(E) -> CommandError> where
+  E : Into<CommandError> {
+    let message = message.into();
+    return Box::new(move |err| {
+        return CommandError::Trace(message, Box::new(err.into()));
+    });
 }
 
 pub fn error(message: impl Into<String>) -> CommandError {
